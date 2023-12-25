11dd6c033fd6a55f6bb53f730f024a5bc8b5fb43
Saidakbar Zafarov <saidakbar.dev@gmail.com>
2023-12-25 14:58:46 +0500

    removed unused dependency

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,15 +1,7 @@
 const userService = require('../service/user-service')
-const { validationResult } = require('express-validator')
-
 class UserController {
     async registration(req, res, next) {
         try {
-            const errors = validationResult(req)
-
-            if (!errors.isEmpty()) {
-                return console.error('Validation Error: ', errors)
-            }
-
             const { username, password, email } = req.body
             const userData = await userService.registration(username, password, email)
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
